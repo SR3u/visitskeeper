446b2ff59706a18ea467dcbcc5340801753ca7ca
SR3u <wnmsr3u@gmail.com>
2024-02-28 17:18:39 +0300

    initial

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DIR=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 cd "$DIR"
-git clone https://SR3u@bitbucket.org/visitkeeper/backend.git
+git clone https://github.com/SR3u/visitskeeper.git
 cd ./backend
 git fetch
 git pull origin master
