2faa4e1632dc8742b2b17e934778f6c200736401
SR3u <wnmsr3u@gmail.com>
2026-09-02 17:54:58 +0300

    better frontend: dark mode

--- src/main/frontend/showvisitskeeper/src/GoogleSearchBar.jsx
+++ src/main/frontend/showvisitskeeper/src/GoogleSearchBar.jsx
@@ -143,16 +143,18 @@ const GoogleSearchBar = () => {
             <Box sx={{flexGrow: 1}}>
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{mr: 2}}
-                            onClick={toggleDrawer(true)}
-                        >
-                            <SearchIcon/>
-                        </IconButton>
+                        <Tooltip title="Поиск">
+                            <IconButton
+                                size="large"
+                                edge="start"
+                                color="inherit"
+                                aria-label="menu"
+                                sx={{mr: 2}}
+                                onClick={toggleDrawer(true)}
+                            >
+                                <SearchIcon/>
+                            </IconButton>
+                        </Tooltip>
                         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                             {header}
                         </Typography>
